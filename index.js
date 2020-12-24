25c7706331ee0776e7a64571f6dd5d78c4c025f7
Martin Smith <martin.smifff@gmail.com>
2020-12-24 10:47:09 +0000

    Setup first route

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const path = require('path');
 const express = require("express");
 const app = express(); // create express app
 
-
-app.use(express.static(path.join(__dirname, "..", "build")));
-
+app.use('/', (req, res) => {
+  res.json({
+    message: "Hello"
+  })
+})
 
 // start express server on port 5000
 app.listen(5000, () => {
